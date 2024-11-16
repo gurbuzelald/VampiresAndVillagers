e3836efe7f4331d98ab88f5e1167539cb5018b94
Gürbüz <73827791+gurbuzelald@users.noreply.github.com>
2024-11-16 14:46:15 +0300

    Added PlayerController

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+
+    public Transform cameraTransform;
+    public float lookSpeed = 2f;
+
+    private Rigidbody _rigidbody;
+
+    [SerializeField] LayerMask layerMask;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleCameraRotation();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HandleJump();
+        }
+    }
+
+    private void HandleMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontal, 0f, vertical);
+
+        if (moveDirection.magnitude > 0.1f)
+        {
+            moveDirection.Normalize();
+
+            moveDirection = cameraTransform.forward * moveDirection.z + cameraTransform.right * moveDirection.x;
+            moveDirection.y = 0f;
+
+            if (!IsGrounded())
+            {
+                transform.Translate(moveDirection * moveSpeed / 3 * Time.deltaTime, Space.World);
+            }
+            else
+            {
+                transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
+            }
+        }
+    }
+
+
+    private void HandleCameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(Vector3.up, mouseX * lookSpeed);
+
+        cameraTransform.Rotate(Vector3.left, mouseY * lookSpeed);
+    }
+
+    private void HandleJump()
+    {
+        if (IsGrounded())
+        {
+            _rigidbody.AddForce(Vector3.up * jumpForce * Time.deltaTime * 100, ForceMode.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 1.1f, layerMask);
+    }
+}
