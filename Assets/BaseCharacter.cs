a4e148f5f309df568fa033839942576b58383d12
Gürbüz <73827791+gurbuzelald@users.noreply.github.com>
2024-11-17 11:59:13 +0300

    Human Movement States moved to HumanController

--- Assets/BaseCharacter.cs
+++ Assets/BaseCharacter.cs
@@ -13,81 +13,11 @@ public class BaseCharacter : MonoBehaviour
 
     public State currentState;
 
-    [SerializeField] private float hiddenTime;
+    public float hiddenTime;
 
     public Transform _hideAreasObject;
     public Transform[] hideAreas;
 
-    private int currentHideAreaIndex = 0;
+    public int currentHideAreaIndex = 0;
 
-    private void Awake()
-    {
-        currentState = State.Patrolling;
-        currentHideAreaIndex = 0;
-    }
-
-
-    public void HandleStates(RaycastHit[] hits, ref Vector3 currentTargetPosition, Transform patrolTarget)
-    {
-        switch (currentState)
-        {
-            case State.Patrolling:
-                Patrol(ref currentTargetPosition, patrolTarget);
-                CheckForVampires(hits); // Transition to Escaping if vampires are detected
-                break;
-
-            case State.Escaping:
-                Escape(ref currentTargetPosition);
-                break;
-
-            case State.Hiding:
-                break;
-        }
-    }
-
-    private void Patrol(ref Vector3 currentTargetPosition, Transform patrolTarget)
-    {
-        currentTargetPosition = patrolTarget.position;
-    }
-
-    private void Escape(ref Vector3 currentTargetPosition)
-    {
-        float nearestDistance = Mathf.Infinity;
-
-        for (int i = 0; i < hideAreas.Length; i++)
-        {
-            float tempDistance = Vector3.Distance(transform.position, hideAreas[i].position);
-            if (tempDistance < nearestDistance)
-            {
-                nearestDistance = tempDistance;
-                currentHideAreaIndex = i;
-            }
-        }
-
-        currentTargetPosition = hideAreas[currentHideAreaIndex].position;
-
-        if (Vector3.Distance(transform.position, hideAreas[currentHideAreaIndex].position) < 0.1f)
-        {
-            currentState = State.Hiding;
-            StartCoroutine(DeactivateHiding(hiddenTime));
-        }
-    }
-
-    private void CheckForVampires(RaycastHit[] hits)
-    {
-        foreach (var hit in hits)
-        {
-            if (hit.collider.CompareTag("Vampire"))
-            {
-                currentState = State.Escaping;
-                return;
-            }
-        }
-    }
-
-    private IEnumerator DeactivateHiding(float hiddenTime)
-    {
-        yield return new WaitForSeconds(hiddenTime);
-        currentState = State.Patrolling;
-    }
 }
--- Assets/HumanController.cs
+++ Assets/HumanController.cs
@@ -23,6 +23,9 @@ public class HumanController : BaseCharacter
 
     private void Awake()
     {
+        currentState = State.Patrolling;
+        currentHideAreaIndex = 0;
+
         pointsSingleton = FindAnyObjectByType<PointsSingleton>();
 
         _hideAreasObject = pointsSingleton.hideAreasObject;
@@ -80,5 +83,70 @@ public class HumanController : BaseCharacter
     private RaycastHit[] CheckAround(Transform _transform, float radius, LayerMask layerMask)
     {
         return Physics.SphereCastAll(_transform.position, radius, _transform.forward, radius, layerMask);
+    }
+
+
+    public void HandleStates(RaycastHit[] hits, ref Vector3 currentTargetPosition, Transform patrolTarget)
+    {
+        switch (currentState)
+        {
+            case State.Patrolling:
+                Patrol(ref currentTargetPosition, patrolTarget);
+                CheckForVampires(hits); // Transition to Escaping if vampires are detected
+                break;
+
+            case State.Escaping:
+                Escape(ref currentTargetPosition);
+                break;
+
+            case State.Hiding:
+                break;
+        }
+    }
+
+    private void Patrol(ref Vector3 currentTargetPosition, Transform patrolTarget)
+    {
+        currentTargetPosition = patrolTarget.position;
+    }
+
+    private void Escape(ref Vector3 currentTargetPosition)
+    {
+        float nearestDistance = Mathf.Infinity;
+
+        for (int i = 0; i < hideAreas.Length; i++)
+        {
+            float tempDistance = Vector3.Distance(transform.position, hideAreas[i].position);
+            if (tempDistance < nearestDistance)
+            {
+                nearestDistance = tempDistance;
+                currentHideAreaIndex = i;
+            }
+        }
+
+        currentTargetPosition = hideAreas[currentHideAreaIndex].position;
+
+        if (Vector3.Distance(transform.position, hideAreas[currentHideAreaIndex].position) < 0.1f)
+        {
+            currentState = State.Hiding;
+            StartCoroutine(DeactivateHiding(hiddenTime));
+        }
+    }
+
+    private void CheckForVampires(RaycastHit[] hits)
+    {
+        foreach (var hit in hits)
+        {
+            if (hit.collider.CompareTag("Vampire"))
+            {
+                currentState = State.Escaping;
+                return;
+            }
+        }
+    }
+
+    private IEnumerator DeactivateHiding(float hiddenTime)
+    {
+        yield return new WaitForSeconds(hiddenTime);
+        currentState = State.Patrolling;
     }
 }
